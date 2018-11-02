fce3ede052af729d2dd81760902c48d9d4d9d3fe
elama09 <roman.tcharni@gmail.com>
2018-11-02 16:28:24 +0200

    fixed pagination li active

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -10,22 +10,22 @@ class Pagination extends Component {
     return (
       <nav>
         <ul class="pagination justify-content-center mt-3 mb-3">
-          <li class="mr-2 { this.props.activePage === 1 ? `page-item active` : `page-item` }" >
+          <li class={this.props.activePage === 1 ? "mr-2 page-item active" : "mr-2 page-item"} >
             <a class="page-link" onClick={this.props.pageOne} href="#">
               1
             </a>
           </li>
-          <li class="mr-2 { this.props.activePage === 2 ? `page-item active` : `page-item` }">
+          <li class={this.props.activePage === 2 ? `mr-2 page-item active` : `mr-2 page-item` }>
             <a class="page-link" onClick={this.props.pageTwo} href="#">
               2
             </a>
           </li>
-          <li class="mr-2 { this.props.activePage === 3 ? `page-item active` : `page-item` }">
+          <li class={this.props.activePage === 3 ? `mr-2 page-item active` : `mr-2 page-item` }>
             <a class="page-link" onClick={this.props.pageThree} href="#">
               3
             </a>
           </li>
-          <li class="mr-2 { this.props.activePage === 4 ? `page-item active` : `page-item` }">
+          <li class={this.props.activePage === 4 ? `mr-2 page-item active` : `mr-2 page-item` }>
             <a class="page-link" onClick={this.props.pageFour} href="#">
               4
             </a>
